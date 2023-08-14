1b250e77d5a00fe9eada34441af6adaf1a4eb3a4
Arin Prajapati <arinprajapati78@gmail.com>
2023-08-14 19:17:48 +0530

    DATA IS SAVED

--- src/constant/constantData.jsx
+++ src/constant/constantData.jsx
@@ -5,25 +5,25 @@ export const tabs = [
     style: "",
   },
   {
-    name: "Audio & Video",
+    name: "Clothing",
     url: "alskdfj",
 
     style: "",
   },
   {
-    name: "Refrigerator",
+    name: "Electronics",
     url: "alskdfj",
   },
   {
-    name: "New arrivals",
+    name: "Beauty Care",
     url: "alskdfj",
   },
   {
-    name: "Today's Deal",
+    name: "Sports Outdoors",
     url: "alskdfj",
   },
   {
-    name: "Gift Cards",
+    name: "Books",
     url: "alskdfj",
   },
   {
@@ -36,12 +36,39 @@ export const tabs = [
   },
 ];
 
-export const tabsMap = {
-  Hpme: {
-    name: "asdj",
+export const productList = [
+  {
+    name: "Home appliances",
+    img: "https://5.imimg.com/data5/OV/KM/CR/ANDROID-37090388/product-jpeg.jpg",
+
+    url: "#",
   },
-  About: {},
-};
+  {
+    name: "Clothing",
+    img: "https://hips.hearstapps.com/hmg-prod/images/index-onlinestores-1669916593.jpg",
+    url: "#",
+  },
+  {
+    name: "Electronics",
+    img: "https://researchsnipers.com/wp-content/uploads/2021/02/electronics.jpeg",
+    url: "#",
+  },
+  {
+    name: "Beauty Care",
+    img: "https://images.everydayhealth.com/images/what-are-natural-skin-care-products-alt-1440x810.jpg",
+    url: "#",
+  },
+  {
+    name: "Sports Outdoors",
+    img: "https://media.istockphoto.com/id/949190736/photo/various-sport-equipments-on-grass.jpg?s=612x612&w=0&k=20&c=e5XgszJQciKRrqQECO9RPqLh7w1kkhNBFetf4742BF0=",
+    url: "#",
+  },
+  {
+    name: "Books",
+    img: "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop",
+    url: "#",
+  },
+];
 
 export const heroStyle = {
   backgroundImage: "url(/background.jpg)",
@@ -58,51 +85,62 @@ export const heroStyle = {
 };
 
 export const star = {
-  zero: (<div className="online">
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-</div> ),
+  zero: (
+    <div className="online">
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+    </div>
+  ),
 
-  one:  (<div className="online">
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-</div> ),
+  one: (
+    <div className="online">
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+    </div>
+  ),
 
-  two:  (<div className="online">
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-</div> ),
+  two: (
+    <div className="online">
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+    </div>
+  ),
 
-  three:
-  (<div className="online">
-  <img src="/fullStar.svg" className="w-[1rem]"  alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-</div> ),
+  three: (
+    <div className="online">
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+    </div>
+  ),
 
-  four:  (<div className="online">
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
-</div> ),
-  five:  (<div className="online">
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-  <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
-</div> ),
+  four: (
+    <div className="online">
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/zeroStar.svg" className="w-[1rem]" alt="zeroStar" />
+    </div>
+  ),
+  five: (
+    <div className="online">
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+      <img src="/fullStar.svg" className="w-[1rem]" alt="fullStar" />
+    </div>
+  ),
 };
